f871107d07f306f826d8269556021c6dd4a4107d
Nathan Telles <me@nathantelles.com>
2022-01-23 16:03:49 -0800

    Moved pynacl to aws lambda layer so it no longer needs to be included in the deployment package!

--- deploy_discord_interactions.sh
+++ deploy_discord_interactions.sh
@@ -2,10 +2,8 @@
 deployment_package=discord_interactions_package.zip
 
 rm -f $deployment_package
-cd .venv/lib/python3.8/site-packages
-zip -r ../../../../$deployment_package .
-cd ../../../../discord_interactions
-zip -g ../$deployment_package *.py
+cd ./discord_interactions
+zip ../$deployment_package *.py
 cd ..
 echo "Deploying to AWS..."
 aws lambda update-function-code --function-name abc_discord_interactions --region us-west-2 --zip-file fileb://$deployment_package
